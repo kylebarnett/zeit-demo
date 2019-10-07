42db194c4038e4c98b4bd891f2f59dac4e624f2e
Kyle Barnett <kbarnett23@gmail.com>
2019-10-07 12:16:20 -0600

    added h2

--- src/App.js
+++ src/App.js
@@ -16,15 +16,7 @@ function App() {
     <main>
       <h1>Create React App + Go API</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          ZEIT Now
-        </a>
-        !
+        WEB PT 7
       </h2>
       <p>
         <a
